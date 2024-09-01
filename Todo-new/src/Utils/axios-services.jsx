241dcb3b6efdb55ad6c81459749bcae3320cd268
Tooba-Shafique <tshafique234@gmail.com>
2024-09-01 17:01:12 +0500

    Update axios-services.jsx

--- Todo-new/src/Utils/axios-services.jsx
+++ Todo-new/src/Utils/axios-services.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "https://todo-app-backend-one-rosy.vercel.app" //"http://localhost:3001"; //"http://162.255.117.211/api" //
+const baseURL = "https://todo-app-seven-liart-25.vercel.app" //"http://localhost:3001"; //"http://162.255.117.211/api" //
 const apiService = axios.create({
   baseURL,
   withCredentials: true
